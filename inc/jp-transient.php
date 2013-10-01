7a0bc3fe3a4eaa30e8e5725e6eed2a5bdbd26b77
Josh <JPollock412@gmail.com>
2013-10-01 03:05:15 -0400

    initial commit

---
+++ inc/jp-transient.php
@@ -0,0 +1,141 @@
+<?php
+/**
+* @author Josh Pollock (http://JoshPress.net)
+*/
+
+
+/**
+* Class To Store and Retrieve Results Of Functions In WordPress Transient Cache
+*
+* http://codex.wordpress.org/Transients_API
+*
+* @author Josh Pollock
+* @since 0.1
+*/
+
+class jp_transient{
+
+	/**
+	* Save To Transient Cache
+	*
+	* @param $value Value to be saved.
+	* @param $name Name of what we are saving.
+	* @param string|int $reset (optional) How long to keep in cache. Options string: minute|hour|day|week|year|none or exact number of seconds as integer
+	* @todo Set expiration time using reset method
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+	
+	public function set( $value, $name, $reset ) {
+	
+		/* prepare value for $reset */
+		//translate value of $reset into 
+		if ( $reset = 'minute' ) {
+			$reset = MINUTE_IN_SECONDS;
+		}
+		elseif ( $reset = 'hour' ) {
+			$reset = HOUR_IN_SECONDS;
+		}
+		elseif ( $reset = 'day' ) {
+			$reset = DAY_IN_SECONDS;
+		}
+		elseif ( $reset = 'week' ) {
+			$reset = WEEK_IN_SECONDS;
+		}
+		elseif ( $reset = 'year' ) {
+			$reset = YEAR_IN_SECONDS;
+		}
+		//other situations
+		elseif ( is_int( $reset ) ) {
+			//if its an integer leave it alone
+			$reset = $reset;
+		}
+		elseif ( is_empty( $reset ) ) {
+			//if its empty set it to be false
+			$reset = fales;
+		}
+		else {
+			//for safety's sake
+			$reset = false;
+		}
+		
+		/* Set the transient */
+		//test if we have anything to set.
+		if ( ! empty( $value ) ) {
+			if ( is_multisite() ) {
+				if ( $reset != false ) {
+					set_site_transient( $name, $value, $reset );
+				}
+				else {
+					set_site_transient( $name, $value );
+				}
+			} //is_multisite
+			else {
+				//skip setting a rest time if we don't have one to set.
+				if ( $reset != false ) {
+						set_transient( $name, $value, $reset );
+					}
+				else {
+					set_transient( $name, $value );
+				}
+			} // if ! is_multisite
+		} // ! empty( $value )
+	}
+	
+	/**
+	* Get From Transient Cache
+	*
+	* @param $name Name of value (saved with set method) to be retrieved
+	* @returns $data Vale from transient cache.
+	* @todo more params to customize what we get out, array vs object, specific keys etc.
+	* 
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+	public function get( $name ) {
+		
+		//get the transient
+		if ( is_multisite() ) {
+				$transient = get_site_transient( $name );
+			}
+		else {
+			$transient = get_transient( $name );
+		}
+		
+		//return value if possible.
+		if ( empty( $transient ) ){
+			//we have a transient so return value
+			$transient = $data;
+		}
+		else {
+			//nothing to return so return false
+			$date = false;
+		}
+		
+		//output our data.
+		return $data;
+
+	}
+	
+	
+	/**
+	* Reset Transients
+	*
+	* @todo This. Idea is to be able to use it to more easily set reset times for transients.
+	* @todo Reset at specific time.
+	* @todo Put site into maintenance mode while rebuilding cache.
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+
+	public function auto_reset() {
+		//Do this later
+
+	}
+
+
+
+}
---
+++ plugin.php
@@ -0,0 +1,165 @@
+<?php
+/*
+* @package jp-multisite-links
+* @author Josh Pollock
+* @since 0.1
+*/
+
+/*
+Plugin Name: Multisite Lists
+Plugin URI: 
+Description: Creates lists of things in a multisite network, like blogs, posts, pages etc. Uses transient cache to speed up list display.
+Version: 0.1
+Author: Josh Pollock
+Author URI: http://JoshPress.net
+License: GNU General Public License v2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/**
+* Include the jp-transient class
+*
+* @returns jp-transient class
+* @since 0.1
+* @author Josh Pollock
+*/
+
+include( 'inc/jp-transient.php' );
+function bl_trans() { 
+	$trans = new jp_transient();
+	return $trans;
+}
+
+/**
+* Creates the array of blog information, posts, pages
+*
+* @returns array @site_info {
+		'blog_id'
+		'blog_url'
+		'blog_name'	
+		'post'
+		'pages'
+* }
+* @package jp-multisite-links
+* @author Josh Pollock
+* @since 0.1
+*/
+function bl_blog_info() {
+	global $blog_id;
+	$sites = wp_get_sites();
+	$sites_info = array();
+	$current_site = $blog_id;
+
+	//run throught the sites. switch to blogs, get pages and posts as well as blog name, ID and url
+	foreach ( $sites as $site ) {
+		switch_to_blog( $site[ 'blog_id' ] );
+		$posts = get_posts();
+		$pages = get_pages();
+		$sites_info[] = array(
+			'blog_id'		=> $site[ 'blog_id' ],
+			'blog_url'		=> get_home_url(),
+			'blog_name'		=> get_bloginfo( 'name'),
+			'postss'		=> $posts,
+			'pages'			=> $pages
+		);
+	}
+	
+	//return the array
+	return $sites_info;
+}
+
+/**
+* List all posts from network
+*
+* @package jp-multisite-links
+* @author Josh Pollock
+* @since 0.1
+*/
+function bl_network_posts() {
+	$blogs = bl_blog_info();
+	foreach ($blogs as $blog) {
+		$name = $blog[ 'blog_name' ];
+		$url = $blog[ 'blog_url' ];
+		switch_to_blog( 'blog_id' );
+		$posts = $blog[ 'posts' ];
+		echo '<h5><a href="'.$url.'">'.$name.'</a></h5>';
+		echo '<ul>';
+		foreach ($posts as $post ) {
+			$id = $post;
+			echo '<li>';
+			$title = get_the_title( $id );
+			$link = get_permalink( $id );
+			echo '<a href="'.$link.'">'.$title.'</a>';
+			echo '</li>';
+		}
+		echo '</ul>';
+	}
+}
+
+/**
+* List all pages from network
+*
+* @package jp-multisite-links
+* @author Josh Pollock
+* @since 0.1
+*/
+function bl_network_pages() {
+	$blogs = bl_blog_info();
+	foreach ($blogs as $blog) {
+		$name = $blog[ 'blog_name' ];
+		$url = $blog[ 'blog_url' ];
+		switch_to_blog( 'blog_id' );
+		$pages = $blog[ 'pages' ];
+		echo '<h5><a href="'.$url.'">'.$name.'</a></h5>';
+		echo '<ul>';
+		foreach ($pages as $page ) {
+			$id = $page;
+			echo '<li>';
+			$title = get_the_title( $id );
+			$link = get_permalink( $id );
+			echo '<a href="'.$link.'">'.$title.'</a>';
+			echo '</li>';
+		}
+		echo '</ul>';
+	}
+}
+
+/**
+* List all posts and pages from network
+*
+* @package jp-multisite-links
+* @author Josh Pollock
+* @since 0.1
+*/
+function bl_network_posts_pages() {
+	$blogs = bl_blog_info();
+	foreach ($blogs as $blog) {
+		$name = $blog[ 'blog_name' ];
+		$url = $blog[ 'blog_url' ];
+		switch_to_blog( 'blog_id' );
+		$posts = $blog[ 'posts' ];
+		echo '<h5><a href="'.$url.'">'.$name.'</a></h5>';
+		echo '<h6>Posts</h6>';
+		echo '<ul>';
+		foreach ($posts as $post ) {
+			$id = $post;
+			echo '<li>';
+			$title = get_the_title( $id );
+			$link = get_permalink( $id );
+			echo '<a href="'.$link.'">'.$title.'</a>';
+			echo '</li>';
+		}
+		echo '</ul></h6>';
+		echo '<h6>pages</h6>';
+		echo '<ul>';
+		foreach ($pages as $page ) {
+			$id = $page;
+			echo '<li>';
+			$title = get_the_title( $id );
+			$link = get_permalink( $id );
+			echo '<a href="'.$link.'">'.$title.'</a>';
+			echo '</li>';
+		}
+		echo '</ul></h6>';
+	}
+}
