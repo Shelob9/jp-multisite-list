d1adeb861cb82d28f6cc7cea408f1f246d456865
Josh <JPollock412@gmail.com>
2013-10-01 03:58:32 -0400

    refactor plugin.php as a class with better layout

--- plugin.php
+++ plugin.php
@@ -26,149 +26,153 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 include( 'inc/jp-transient.php' );
 
 
-/**
-* Creates the array of blog information, posts, pages
-*
-* @returns array @site_info {
-		'blog_id'
-		'blog_url'
-		'blog_name'	
-		'post'
-		'pages'
-* }
-* @package jp-multisite-links
-* @author Josh Pollock
-* @since 0.1
-*/
-function bl_blog_info() {
-	global $blog_id;
-	$sites = wp_get_sites();
-	$sites_info = array();
-	$current_site = $blog_id;
+class jp_multisite_list {
 
-	//run throught the sites. switch to blogs, get pages and posts as well as blog name, ID and url
-	foreach ( $sites as $site ) {
-		switch_to_blog( $site[ 'blog_id' ] );
-		$posts = get_posts();
-		$pages = get_pages();
-		$sites_info[] = array(
-			'blog_id'		=> $site[ 'blog_id' ],
-			'blog_url'		=> get_home_url(),
-			'blog_name'		=> get_bloginfo( 'name'),
-			'posts'		=> $posts,
-			'pages'			=> $pages
-		);
+	/**
+	* Creates the array of blog information, posts, pages
+	*
+	* @returns array @site_info {
+			'blog_id'
+			'blog_url'
+			'blog_name'	
+			'post'
+			'pages'
+	* }
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+	public function blog_info() {
+		global $blog_id;
+		$sites = wp_get_sites();
+		$sites_info = array();
+		$current_site = $blog_id;
+
+		//run throught the sites. switch to blogs, get pages and posts as well as blog name, ID and url
+		foreach ( $sites as $site ) {
+			switch_to_blog( $site[ 'blog_id' ] );
+			$posts = get_posts();
+			$pages = get_pages();
+			$sites_info[] = array(
+				'blog_id'		=> $site[ 'blog_id' ],
+				'blog_url'		=> get_home_url(),
+				'blog_name'		=> get_bloginfo( 'name'),
+				'posts'		=> $posts,
+				'pages'			=> $pages
+			);
+		}
+	
+		//return the array
+		return $sites_info;
+	}
+
+	/**
+	* List all posts from network
+	*
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+	public function posts() {
+		$test = jp_transient::get('jp_posts');
+		//if there is nothing in cache assemble output
+		if ( $test == false) {
+			//create out put for posts
+			$list = posts_list();
+			//cache it for next time
+			jp_transient::set( 'posts', $out );
+		}
+		else {
+			$out = jp_transient::get( 'jp_posts' );
+		}
+		return $out;
 	}
 	
-	//return the array
-	return $sites_info;
-}
-
-/**
-* List all posts from network
-*
-* @package jp-multisite-links
-* @author Josh Pollock
-* @since 0.1
-*/
-function bl_network_posts() {
-	$test = jp_transient::get('bl_posts');
-	//if there is nothing in cache assemble output
-	if ( $test == false) {
-		$out = '';
-		$blogs = bl_blog_info();
+	/**
+	* Create post list
+	*
+	* @returns string $posts_list the formatted post list
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+	public function posts_list() {
+		$data = '';
+		$blogs = blog_info();
 		foreach ($blogs as $blog) {
 			$name = $blog[ 'blog_name' ];
 			$url = $blog[ 'blog_url' ];
+			$data .= '<h5><a href="'.$url.'">'.$name.'</a></h5>';
 			switch_to_blog( 'blog_id' );
 			$posts = $blog[ 'posts' ];
-			$out .= '<h5><a href="'.$url.'">'.$name.'</a></h5>';
-			$out .= '<ul>';
+			
+			$data .= '<ul>';
 			foreach ($posts as $post ) {
 				$id = $post;
-				$out .= '<li>';
+				$data .= '<li>';
 				$title = get_the_title( $id );
 				$link = get_permalink( $id );
-				$out .= '<a href="'.$link.'">'.$title.'</a>';
-				$out .= '</li>';
+				$data .= '<a href="'.$link.'">'.$title.'</a>';
+				$data .= '</li>';
 			}
-			$out .= '</ul>';
+			$data .= '</ul>';
+		}
+		$posts_list = $data;
+	}
+	
+	/**
+	* List all pages from network
+	*
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+	public function pages() {
+		$test = jp_transient::get('jp_pages');
+		//if there is nothing in cache assemble output
+		if ( $test == false) {
+			//create out put for pages
+			$list = pages_list();
 			//cache it for next time
-			jp_transient::set( 'bl_posts', $out );
+			jp_transient::set( 'pages', $out );
 		}
+		else {
+			$out = jp_transient::get( 'jp_pages' );
+		}
+		return $out;
 	}
-	else {
-		$out =jp_transient::get( 'bl_posts' );
+			
+		/**
+	* Create page list
+	*
+	* @returns string $pages_list the formatted page list
+	* @package jp-multisite-links
+	* @author Josh Pollock
+	* @since 0.1
+	*/
+	public function pages_list() {
+		$data = '';
+		$blogs = blog_info();
+		foreach ($blogs as $blog) {
+			$name = $blog[ 'blog_name' ];
+			$url = $blog[ 'blog_url' ];
+			$data .= '<h5><a href="'.$url.'">'.$name.'</a></h5>';
+			switch_to_blog( 'blog_id' );
+			$pages = $blog[ 'pages' ];
+			
+			$data .= '<ul>';
+			foreach ($pages as $page ) {
+				$id = $page;
+				$data .= '<li>';
+				$title = get_the_title( $id );
+				$link = get_permalink( $id );
+				$data .= '<a href="'.$link.'">'.$title.'</a>';
+				$data .= '</li>';
+			}
+			$data .= '</ul>';
+		}
+		$pages_list = $data;
 	}
-	//echo
-	echo $out;
 }
 
-/**
-* List all pages from network
-*
-* @package jp-multisite-links
-* @author Josh Pollock
-* @since 0.1
-*/
-function bl_network_pages() {
-	$blogs = bl_blog_info();
-	foreach ($blogs as $blog) {
-		$name = $blog[ 'blog_name' ];
-		$url = $blog[ 'blog_url' ];
-		switch_to_blog( 'blog_id' );
-		$pages = $blog[ 'pages' ];
-		echo '<h5><a href="'.$url.'">'.$name.'</a></h5>';
-		echo '<ul>';
-		foreach ($pages as $page ) {
-			$id = $page;
-			echo '<li>';
-			$title = get_the_title( $id );
-			$link = get_permalink( $id );
-			echo '<a href="'.$link.'">'.$title.'</a>';
-			echo '</li>';
-		}
-		echo '</ul>';
-	}
-}
-
-/**
-* List all posts and pages from network
-*
-* @package jp-multisite-links
-* @author Josh Pollock
-* @since 0.1
-*/
-function bl_network_posts_pages() {
-	$blogs = bl_blog_info();
-	foreach ($blogs as $blog) {
-		$name = $blog[ 'blog_name' ];
-		$url = $blog[ 'blog_url' ];
-		switch_to_blog( 'blog_id' );
-		$posts = $blog[ 'posts' ];
-		$pages = $blog[ 'pages' ];
-		echo '<h5><a href="'.$url.'">'.$name.'</a></h5>';
-		echo '<h6>Posts</h6>';
-		echo '<ul>';
-		foreach ($posts as $post ) {
-			$id = $post;
-			echo '<li>';
-			$title = get_the_title( $id );
-			$link = get_permalink( $id );
-			echo '<a href="'.$link.'">'.$title.'</a>';
-			echo '</li>';
-		}
-		echo '</ul></h6>';
-		echo '<h6>pages</h6>';
-		echo '<ul>';
-		foreach ($pages as $page ) {
-			$id = $page;
-			echo '<li>';
-			$title = get_the_title( $id );
-			$link = get_permalink( $id );
-			echo '<a href="'.$link.'">'.$title.'</a>';
-			echo '</li>';
-		}
-		echo '</ul></h6>';
-	}
-}
+new jp_multisite_list();
